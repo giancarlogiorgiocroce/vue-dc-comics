9e1b91d7c2b7a3f7c038bcc82c71b8225d34b620
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-13 16:01:04 +0200

    Compleato Header (manca solamente il comando per dare l'active)

--- vue-dc-comics/src/App.vue
+++ vue-dc-comics/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    
+    <HeaderComp />
   </div>
 </template>
 
 <script>
+import HeaderComp from "./components/HeaderComp.vue";
 
 export default {
   name: 'App',
   components: {
- 
-  }
+    HeaderComp,
+}
 }
 </script>
 
 <style lang="scss">
+@import 'assets/style/vars.scss';
+@import 'assets/style/global.scss';
 
 </style>
---
+++ vue-dc-comics/src/components/HeaderComp.vue
@@ -0,0 +1,109 @@
+<template>
+  <header>
+      <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="DC logo">
+      </div>
+      <nav>
+          <ul>
+              <li v-for="(el, i) in navbar" :key="`navbar${i}`">
+                  <a href="">{{el.name}}</a>
+              </li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        navbar:[
+            {
+                name: "characters",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "comics",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "movies",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "tv",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "games",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "collectibles",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "videos",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "fans",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "news",
+                href: "#",
+                isActive: false,
+            },
+            {
+                name: "shop",
+                href: "#",
+                isActive: false,
+            },
+        ],
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/vars';
+    header{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 100px;
+        img{
+            height: 75px;
+        };
+    }
+    ul{
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        li {
+            padding: 50px 0;
+            margin: 0 15px;
+            &.active{
+                border-bottom: 5px solid rgb(2, 130, 249);
+            }
+        }
+        a {
+            text-transform: uppercase;
+            font-size: 12px;
+            font-weight: bold;
+            &:hover{
+                color: rgb(2, 130, 249);
+            }
+        }
+    }
+</style>
