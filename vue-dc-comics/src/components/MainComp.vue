8a783a634b0877a8dbd15c3c8bf14274b411fb03
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-16 14:56:29 +0200

    Utilizzato il prop per ciclare i vari box fumetti

--- vue-dc-comics/src/components/MainComp.vue
+++ vue-dc-comics/src/components/MainComp.vue
@@ -1,24 +1,38 @@
 <template>
   <main>
       <div class="container">
-          <h1>---->Content goes here----></h1>
+          <SingleSlide 
+            :key="i"
+            v-for="(el, i) in arrayComics"
+            :comic="el"/>
       </div>
   </main>
 </template>
 
 <script>
-export default {
+import SingleSlide from './SingleSlide.vue';
+import dcComics from '../assets/data/dc-comics.json';
 
+export default {
+    components: { SingleSlide },
+    data(){
+        return{
+            arrayComics: dcComics,
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 main{
     margin-top: 1.5px;
-    height: 100px;
     background-color: black;
-    line-height: 100px;
-    
+
+    .container{
+        display: flex;
+        flex-wrap: wrap;
+    }
+
     h1{
         color: white;
         font-size: 30px;
---
+++ vue-dc-comics/src/components/SingleSlide.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+      <div class="img">
+          <img :src="comic.thumb" :alt="comic.series">
+      </div>
+      <h5>{{comic.series}}</h5>
+  </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        comic: Object,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.box{
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    flex-basis: calc((100% / 6) - 20px);
+    margin: 10px;
+
+    .img{
+        width: 21vh;
+        height: 21vh;
+        overflow: hidden;
+        img{
+            width: 100%;
+        }
+    };
+}
+
+h5{
+    color: lightgrey;
+    text-transform: uppercase;
+    margin-top: 20px;
+}
+
+</style>
